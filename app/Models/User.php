d54af6f2217d938e6634afc8e08cfa6ed2874bd5
YebasMardo <yebasmardo@gmail.com>
2026-04-28 10:21:57 +0100

    Fixing cors

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements JWTSubject
 {
@@ -30,7 +30,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function isAdmin()
     {
-        return $this->role = 'admin';
+        return $this->role === 'admin';
     }
 
     /**
